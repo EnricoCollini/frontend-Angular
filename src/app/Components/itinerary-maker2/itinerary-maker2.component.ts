a8dddeb62f12a9182d852ec397a0c8027945c116
enrico <encollini@gmail.com>
2020-11-24 13:35:26 +0100

    createItinerarioStarted

--- src/app/Components/itinerary-maker2/itinerary-maker2.component.ts
+++ src/app/Components/itinerary-maker2/itinerary-maker2.component.ts
@@ -10,6 +10,12 @@ import { MapElementsService } from 'src/app/Services/mapElementsService/map-elem
   styleUrls: ['./itinerary-maker2.component.css']
 })
 export class ItineraryMaker2Component implements OnInit {
+  private isItinerarioPresente = false;
+  private isPartenzaSet = false;
+  private isArrivoSet = false;
+  private numberPuntiIntermediSet = 0;
+  public itinerario = [];
+
   public map: L.Map;
   public zoom: number;
   public  startIcon : L.Icon;
@@ -56,6 +62,7 @@ export class ItineraryMaker2Component implements OnInit {
         this.partenza =new L.Marker(new L.LatLng(coords[1], coords[0]),{icon: this.startIcon});
         this.partenza.addTo(this.map);
         this.partenzaPlaceholder = res.search;
+        this.isPartenzaSet = true;
         form.reset();
       }
     );
@@ -73,6 +80,7 @@ export class ItineraryMaker2Component implements OnInit {
         this.arrivo =new L.Marker(new L.LatLng(coords[1], coords[0]),{icon: this.endIcon});
         this.arrivo.addTo(this.map);
         this.arrivoPlaceholder = res.search;
+        this.isArrivoSet = true;
         form.reset();
       }
     );
@@ -92,6 +100,7 @@ export class ItineraryMaker2Component implements OnInit {
         this.puntiIntermedi[elementId%1000] = this.tmpMarker;
         this.placeholders[elementId%1000] = res.search;
         this.puntiIntermedi[elementId%1000].addTo(this.map);
+        this.numberPuntiIntermediSet = this.numberPuntiIntermediSet + 1;
         form.reset();
       }
     );
@@ -101,11 +110,13 @@ export class ItineraryMaker2Component implements OnInit {
     if(value == 999){ //partenza
       this.map.removeLayer(this.partenza);
       this.partenzaPlaceholder =  "SetStart: Via, Città, ...";
+      this.isPartenzaSet = false;
       form.reset();
     }else{
       if(value == 2000){ //arrivo
         this.map.removeLayer(this.arrivo);
         this.arrivoPlaceholder =  "SetFinish: Via, Città, ...";
+        this.isArrivoSet = false;
         form.reset();
       }else{
         this.map.removeLayer(this.puntiIntermedi[value%1000]);
@@ -123,6 +134,7 @@ export class ItineraryMaker2Component implements OnInit {
         this.puntiIntermediIds = tmp;
         console.log(this.puntiIntermediIds);
         this.count = this.count -1;
+        this.numberPuntiIntermediSet = this.numberPuntiIntermediSet - 1;
         form.reset();
       }
     }
@@ -135,6 +147,59 @@ export class ItineraryMaker2Component implements OnInit {
     this.placeholders.push(this.tmpPlaceholder);
     console.log(this.puntiIntermediIds);
     this.count = this.count + 1;
+  }
+
+
+
+getRoute(){
+  if(!this.isItinerarioPresente){
+    let markers = [];
+    markers.push(this.partenza);
+    for (let index = 0; index < this.puntiIntermedi.length; index++) {
+      markers.push(this.puntiIntermedi[index]);
+    }
+    markers.push(this.arrivo);
+    for (let i = 0; i < markers.length-1; i++) {
+      let j = i +1
+      let startLat = markers[i].getLatLng().lat;
+      let startLon = markers[i].getLatLng().lng;
+      let endLat = markers[j].getLatLng().lat;
+      let endLon = markers[j].getLatLng().lng;
+      this._itinerarymakerservice.getItinerario(startLon, startLat,endLon , endLat )
+        .subscribe(data => {
+          let itiner = data;
+          let itinerJ= JSON.stringify(itiner);
+          this.itinerario.push(L.geoJSON(JSON.parse(itinerJ)));
+          this.itinerario[this.itinerario.length-1].addTo(this.map);
+        });
+      this.isItinerarioPresente = true;
+    }
+  }else{
+    for (let index = 0; index < this.itinerario.length; index++) {
+      this.map.removeLayer(this.itinerario[index]);  
+    }
+    let markers = [];
+    markers.push(this.partenza);
+    for (let index = 0; index < this.puntiIntermedi.length; index++) {
+      markers.push(this.puntiIntermedi[index]);
+    }
+    markers.push(this.arrivo);
+    for (let i = 0; i < markers.length-1; i++) {
+      let j = i +1
+      let startLat = markers[i].getLatLng().lat;
+      let startLon = markers[i].getLatLng().lng;
+      let endLat = markers[j].getLatLng().lat;
+      let endLon = markers[j].getLatLng().lng;
+      this._itinerarymakerservice.getItinerario(startLon, startLat,endLon , endLat )
+      .subscribe(data => {
+        let itiner = data;
+        let itinerJ= JSON.stringify(itiner);
+        this.itinerario.push(L.geoJSON(JSON.parse(itinerJ)));
+        this.itinerario[this.itinerario.length-1].addTo(this.map);
+      });
+    this.isItinerarioPresente = true;
+    }  
+  }    
   }
 
 }
